987a098c694f7d59dc0aee2524e43a44b2c68c7d
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-28 13:40:08 -0300

    config: configuração das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom"
+
 function App() {
   
   return (
     <div>
       <h1>Buscador Github</h1>
+      <Outlet />
     </div>
   )
-  
+
 }
 
 export default App
