e2f3a9da33d835f8c7a1396be68de870bf20773e
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-11-02 15:04:16 -0300

    feature: integração com a API do github para buscar repositórios

--- src/routes/Repositories.tsx
+++ src/routes/Repositories.tsx
@@ -4,9 +4,33 @@ import { useParams } from 'react-router-dom'
 import classes from './Repositories.module.css'
 import BackButton from '../components/BackButton'
 
+import { RepositoryProps } from '../types/repository'
+
 const Repositories = () => {
 
     const {username} = useParams()
+
+    const [repositories, setRepositories] = useState<RepositoryProps[] | [] | null>(null)
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+    
+        const loadRepositories = async (username: string) => {
+            setIsLoading(true)
+
+            const res = await fetch(`https://api.github.com/users/${username}/repos`)
+
+            const data = await res.json()
+
+            setIsLoading(false)
+
+            console.log(data)
+        }
+
+        loadRepositories(username as string)
+        
+    }, [])
 
     return (
         <div>
---
+++ src/types/repository.ts
@@ -0,0 +1,7 @@
+export type RepositoryProps = {
+    name: string
+    html_url: string
+    language: string
+    stargazes_count: number
+    forks_count: number
+}
